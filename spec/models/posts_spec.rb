94013861d9dee693ae597cd28f0104acb3fe9465
Juakata <ha.uzquiano@hotmail.com>
2020-01-16 15:03:00 -0600

    add edit,update... to post controller

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   before :each do
-    @post1 = Post.new(title: 'Hello');
-    @post2 = Post.new(content: 'Hello');
-    @post3 = Post.new(title: 'Hello', content: 'Hello');
+    @post1 = Post.new(content: 'Content');
+    @post2 = Post.new(title: 'Hello');
+    @post3 = Post.new(title: 'Hello', content: 'Content');
   end
 
   it 'Post must have title' do
