dfdd00717818d2293ca1d8ea09e3574570463598
EmirVatric <vatricemir@gmail.com>
2020-04-22 00:29:12 +0200

    posts done

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,4 +2,20 @@ class PostPolicy < ApplicationPolicy
   def create?
     user.present?
   end
+
+  def show?
+    user.present?
+  end
+
+  def index?
+    user.present?
+  end
+
+  def update?
+    user == record.user
+  end
+
+  def destroy?
+    user == record.user
+  end
 end
